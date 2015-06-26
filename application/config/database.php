b839fb7ebb4f05d75917e33746138f972f73573e
Joshua Rieland <greengiantcoding@icloud.com>
2015-06-26 14:35:46 -0700

    database revert

--- application/config/database.php
+++ application/config/database.php
@@ -64,10 +64,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => 'root',
+	'database' => 'Caffe_Cocina_Schema',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
